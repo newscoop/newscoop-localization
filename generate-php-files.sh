74aa3537b0627db785eff4226557ef46eb9d7e58
Daniel James <daniel@64studio.com>
2014-05-27 10:09:13 +0100

    Put scripts in their own repository, they are deprecated in Newscoop 4.3 onwards

---
+++ generate-php-files.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Script to generate Newscoop translations from .po files.
+
+# Set the temporary directory to use for converting the files
+BUILDPATH=/tmp
+
+# Set the locales you want to convert
+LOCALES="ar be bn cs da de de_AT el en en_GB es fr he hr hu it ka ko ku nl pl pt pt_BR ro ru sh sq sr sv uk zh zh_TW"
+
+cd ${BUILDPATH}/po/
+
+# Begin function that converts the files
+
+function convert2php {
+  localization=$1
+
+# Check that the target directory actually exists
+
+if [ -d ${localization} ]; then
+
+  echo "Changing the ${localization} files to Newscoop style..."
+
+  sed -i 's/# Newscoop translation file/<?php /g' ${localization}/*.po
+  sed -i ':q;N;s/\n/ /g;t q' ${localization}/*.po
+  sed -i 's/ msgid "/\nregGS("/g' ${localization}/*.po
+  sed -i 's/ msgstr "/, "/g' ${localization}/*.po
+  sed -i '/<?php/!s/[ ]*$/);/g' ${localization}/*.po
+  sed -i '$ a\?>' ${localization}/*.po
+
+  echo " Stripping the final newline from the file..."
+
+  for i in ${localization}/*.po; do
+   awk '{q=p;p=$0}NR>1{print q}END{ORS = ""; print p}' $i > $i.tmp
+   rm $i
+  done
+
+  echo " Giving the changed files a .php extension..."
+
+  for i in ${localization}/*.${localization}.po.tmp; do
+   mv "$i" "${i/.${localization}.po.tmp}".php
+  done
+
+  echo " Checking the syntax of the php files for errors..."
+
+  for i in ${localization}/*.php; do
+   php -l "$i"
+  done
+
+else
+
+  echo "The ${localization} directory was not found."
+
+fi
+
+}
+
+# Run the function for the specified locales
+
+for localization in ${LOCALES}; do
+ convert2php ${localization}
+done
+
+echo "Size of the output files is:"
+
+du -h ${BUILDPATH}/po/*
---
+++ generate-po-files.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Script to generate .po files from Newscoop translations.
+
+# Set the temporary directory to use for converting the files
+BUILDPATH=/tmp
+
+# Set the path to the Newscoop localization files
+GITPATH=newscoop/admin-files/lang/
+
+# Set the path to the Newscoop plugins
+PLUGINPATH=plugins/
+
+# Set the locales you want to convert
+LOCALES="ar be bn cs da de de_AT el en en_GB es fr he hr hu it ka ko ku nl pl pt pt_BR ro ru sh sq sr sv uk zh zh_TW"
+
+# Set the plugins you want to convert
+PLUGINS="debate poll recaptcha soundcloud"
+
+echo "Cleaning up any previous builds..."
+
+rm -rf ${BUILDPATH}/po/
+
+echo "Creating the temporary build directory..."
+
+mkdir -p ${BUILDPATH}/po/
+
+echo "Copying the files to the temporary directory..."
+
+for localization in ${LOCALES}; do
+cp -r ${GITPATH}${localization} ${BUILDPATH}/po/
+
+ for plugin in ${PLUGINS};
+ do cp -r ${PLUGINPATH}${plugin}/admin-files/lang/${localization}/plugin_${plugin}.php ${BUILDPATH}/po/${localization}/;
+ done
+
+done
+
+# Begin function that converts the files
+
+function convert2po {
+  localization=$1
+
+cd ${BUILDPATH}/po/
+
+echo "Changing the copied ${localization} files to GNU gettext style..."
+
+sed -i '1s/<?php /# Newscoop translation file/g' ${localization}/*.php
+sed -i 's/regGS(/msgid /g' ${localization}/*.php
+sed -i 's/", "/"\nmsgstr "/g' ${localization}/*.php
+sed -i 's/);/\n/g' ${localization}/*.php
+sed -i 's/?>//g' ${localization}/*.php
+
+echo " Giving the ${localization} files the .${localization}.po extension..."
+
+for i in ${localization}/*.php; do mv "$i" "${i/.php}".${localization}.po; done
+
+}
+
+# Run the function for the specified locales
+
+for localization in ${LOCALES}; do
+	convert2po ${localization}
+done
+
+echo "Size of the output files is:"
+
+du -h ${BUILDPATH}/po/*
